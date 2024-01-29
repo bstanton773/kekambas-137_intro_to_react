aff74adc0ab18cdbda0d3a6957496230040d606b
Brian <bstanton773@gmail.com>
2024-01-29 15:25:41 -0600

    Create a Home Component view and PostCard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,16 @@
 import { useState } from 'react';
 import Navigation from './components/Navigation';
 import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
+import Home from './views/Home';
+
 
 
 export default function App(){
-    const username:string = 'brians';
+
     // const isLoggedIn:boolean = true;
     const [isLoggedIn, setIsLoggedIn] = useState(false);
     // console.log(isLoggedIn);
     // console.log(typeof setIsLoggedIn);
-
-    const posts: {id:number, title:string}[] = [
-        {id: 1, title: 'Happy Monday!'},
-        {id: 2, title: 'React rules!'},
-        {id: 3, title: 'How was your weekend?'}
-    ]
 
     const handleClick = () => {
         // console.log('The button has been clicked!');
@@ -26,9 +21,7 @@ export default function App(){
         <div>
             <Navigation isLoggedIn={isLoggedIn} />
             <Container>
-                <Button variant='primary' onClick={handleClick}>Click Me</Button>
-                <h1>{ isLoggedIn ? 'Hello ' + username : 'Hello and Welcome' }</h1>
-                { posts.map( p =>  <h4 key={p.id}>{p.title}</h4> ) }
+                <Home isLoggedIn={isLoggedIn} handleClick={handleClick} />
             </Container>
         </div>
     )
