4f4b980763a622823bd7d53df30fd0515c9d8116
Brian <bstanton773@gmail.com>
2024-01-31 11:34:38 -0600

    Update Login component with Form and handleform, etc

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,8 +1,48 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import { UserFormDataType } from '../types';
+
 
 type LoginProps = {}
 
 export default function Login({}: LoginProps) {
+    const navigate = useNavigate();
+
+    const [userFormData, setUserFormData] = useState<Partial<UserFormDataType>>({ username: '', password: ''})
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setUserFormData({...userFormData, [e.target.name]: e.target.value})
+    }
+
+    const handleFormSubmit = (e:React.FormEvent) => {
+        e.preventDefault();
+
+        console.log(userFormData);
+
+        navigate('/')
+    }
+
+
     return (
-        <h1 className='text-center'>Hello this is the Login Component</h1>
+        <>
+            <h1 className='text-center'>Log In</h1>
+            <Card>
+                <Card.Body>
+                    <Form onSubmit={handleFormSubmit}>
+
+                        <Form.Label>Username</Form.Label>
+                        <Form.Control name='username' placeholder='Enter Username' value={userFormData.username} onChange={handleInputChange}/>
+
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control name='password' type='password' placeholder='Enter Password' value={userFormData.password} onChange={handleInputChange}/>
+
+                        <Button type='submit' variant='outline-primary' className='w-100 mt-3' >Log In</Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        </>
     )
 }
