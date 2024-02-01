6737178a02af5eee3cdc44cf4fe61a5643d79b0b
Brian <bstanton773@gmail.com>
2024-02-01 15:19:38 -0600

    Add delete post functionality

--- src/lib/apiWrapper.ts
+++ src/lib/apiWrapper.ts
@@ -151,6 +151,23 @@ async function editPostById(token:string, postId:string|number, editedPostData:P
 }
 
 
+async function deletePostById(token:string, postId:string|number): Promise<APIResponse<string>> {
+    let error;
+    let data;
+    try{
+        const response = await apiClientTokenAuth(token).delete(postEndpoint + '/' + postId);
+        data = response.data.success
+    } catch(err) {
+        if (axios.isAxiosError(err)){
+            error = err.response?.data.error
+        } else {
+            error = 'Something went wrong'
+        }
+    }
+    return { error, data }
+}
+
+
 export {
     register,
     login,
@@ -159,4 +176,5 @@ export {
     createPost,
     getPostById,
     editPostById,
+    deletePostById,
 }
--- src/views/EditPost.tsx
+++ src/views/EditPost.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
-import { getPostById, editPostById } from '../lib/apiWrapper';
+import { getPostById, editPostById, deletePostById } from '../lib/apiWrapper';
 import { CategoryType, PostFormDataType, UserType } from '../types';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 
 
 type EditPostProps = {
@@ -17,6 +18,10 @@ export default function EditPost({ currentUser, flashMessage }: EditPostProps) {
     const navigate = useNavigate();
 
     const [postToEditData, setPostToEditData] = useState<PostFormDataType>({title:'', body:''})
+    const [showModal, setShowModal] = useState(false);
+
+    const openModal = () => setShowModal(true);
+    const closeModal = () => setShowModal(false);
     
     useEffect( () => {
         async function getPost(){
@@ -53,6 +58,17 @@ export default function EditPost({ currentUser, flashMessage }: EditPostProps) {
         }
     }
 
+    const handleDeleteClick = async () => {
+        const token = localStorage.getItem('token') || ''
+        const response = await deletePostById(token, postId!);
+        if (response.error){
+            flashMessage(response.error, 'danger')
+        } else {
+            flashMessage(response.data!, 'primary')
+            navigate('/')
+        }
+    }
+
 
     return (
         <>
@@ -65,10 +81,23 @@ export default function EditPost({ currentUser, flashMessage }: EditPostProps) {
                         <Form.Label>Body</Form.Label>
                         <Form.Control name='body' value={postToEditData.body} onChange={handleInputChange} />
                         <Button variant='success' className='mt-3 w-50' type='submit'>Edit Post</Button>
-                        <Button variant='danger' className='mt-3 w-50' >Delete Post</Button>
+                        <Button variant='danger' className='mt-3 w-50' onClick={openModal}>Delete Post</Button>
                     </Form>
                 </Card.Body>
             </Card>
+
+            <Modal show={showModal} onHide={closeModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Delete {postToEditData.title}?</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    Are you sure you want to delete {postToEditData.title}? This action cannot be undone.
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant='secondary' onClick={closeModal}>Close</Button>
+                    <Button variant='danger' onClick={handleDeleteClick}>Delete Post</Button>
+                </Modal.Footer>
+            </Modal>
         </>
     )
 }
